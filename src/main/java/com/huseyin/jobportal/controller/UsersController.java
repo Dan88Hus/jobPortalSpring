ae6cfaf3b6983d3afa18f07ba11127cf457c97c5
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2024-10-20 18:45:13 +0300

    Init Commit

--- src/main/java/com/huseyin/jobportal/controller/UsersController.java
+++ src/main/java/com/huseyin/jobportal/controller/UsersController.java
@@ -2,21 +2,27 @@ package com.huseyin.jobportal.controller;
 
 import com.huseyin.jobportal.entity.Users;
 import com.huseyin.jobportal.entity.UsersType;
+import com.huseyin.jobportal.services.UsersService;
 import com.huseyin.jobportal.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UsersController {
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
     @Autowired //autowired annotation is not required
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
     @GetMapping("/register")
     public String register(Model model) {
@@ -24,6 +30,23 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+
+    }
+@PostMapping("/register/new")
+    public String userRegistration(@Valid Users users, Model model){
+        Optional<Users> optionalUsers = usersService.getUserByEmail(users.getEmail());
+        if(optionalUsers.isPresent()){
+            model.addAttribute("error", "Email already registered");
+            List<UsersType> usersTypes = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersTypes);
+            model.addAttribute("user", new Users());
+            return "register";
+        }
+
+        System.out.println("user:: "+ users);
+        usersService.addNew(users);
+
+    return "dashboard";
 
     }
 }
