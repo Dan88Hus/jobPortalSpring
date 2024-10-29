902df1b769df2e077b60753053f7678ec9d1d5d2
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2024-10-29 18:32:05 +0300

    Init Commit

---
+++ src/main/java/com/huseyin/jobportal/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.huseyin.jobportal.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * once logged in successfully: Retrieve the user object
+ * Check the roles for the user
+ * if so(jobseeker or recruiter role) role then send them to dashboard page
+ */
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        System.out.println("username"+ username + "is logged in");
+        boolean hasJobSeekerRole = authentication.getAuthorities().stream().anyMatch(r->r.getAuthority().equals("Job Seeker"));
+        boolean hasRecruiterRole = authentication.getAuthorities().stream().anyMatch(r->r.getAuthority().equals("Recruiter"));
+        if (hasRecruiterRole || hasJobSeekerRole) {
+            response.sendRedirect("/dashboard/"); // this is correct
+
+
+        }
+
+
+    }
+}
--- src/main/java/com/huseyin/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/huseyin/jobportal/config/WebSecurityConfig.java
@@ -1,12 +1,28 @@
 package com.huseyin.jobportal.config;
 
+import com.huseyin.jobportal.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+    }
     private final String[] publicUrl = {"/",
             "/global-search/**",
             "/register",
@@ -22,12 +38,38 @@ public class WebSecurityConfig {
             "/*.js.map",
             "/fonts**", "/favicon.ico", "/resources/**", "/error"};
 
+
+
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
         http.authorizeHttpRequests(auth ->{
             auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
-        return http.build();
+        http.formLogin(form->form.loginPage("/login").permitAll().successHandler(customAuthenticationSuccessHandler))
+                .logout(logout->{
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults()).csrf(csrf->csrf.disable());
+
+    return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+//        authenticationProvider is our custom authentication provider
+//        tell springframework how to find our users and how to authenticate passwords
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService); // how to retrieve the users from db
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        tell spring security how to authenticate passwords (plain text or encryption)
+        return new BCryptPasswordEncoder();
+
     }
 }
